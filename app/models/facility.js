23079d9dfbe036da33c566cb11c765af464ed990
Jeremy Gaither <jeremy@jeremygaither.com>
2016-06-04 20:31:29 -0500

    Add geolocation

--- app/models/facility.js
+++ app/models/facility.js
@@ -6,7 +6,8 @@ var Facility = mongoose.model('Facility', new mongoose.Schema({
   zip:  String,
   campaign_description: String,
   help_needed: String,
-  description: String
+  description: String,
+  geolocation: mongoose.Schema.Types.Point  
 }))
 
 module.exports = Facility;
